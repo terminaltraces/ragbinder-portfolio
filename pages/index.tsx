edca93264978743e7fbcf9d2d1a5140eb93596e1
Drew Kozmary <2223678+terminaltraces@users.noreply.github.com>
2023-04-05 17:01:16 -0500

    Merging prev. iteration of project and codespaces work.

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import { Heading } from "@chakra-ui/react";
+
+const IndexPage: NextPage = () => {
+    return <Heading>Hello World</Heading>;
+
+};
+
+export default IndexPage;
