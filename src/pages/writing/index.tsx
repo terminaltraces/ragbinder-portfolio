088000917df236358ac6144f546142e8086145d8
Drew Kozmary <2223678+terminaltraces@users.noreply.github.com>
2024-02-22 20:32:01 -0600

    Removing extra imports from projects and writing

--- src/pages/writing/index.tsx
+++ src/pages/writing/index.tsx
@@ -1,16 +1,4 @@
 import { NextPage } from "next";
-import {
-  Heading,
-  Text,
-  Card,
-  CardHeader,
-  SimpleGrid,
-  CardBody,
-  CardFooter,
-  Image,
-  Stack,
-  Button,
-} from "@chakra-ui/react";
 
 const WritingsPage: NextPage = () => {
   return <></>;
