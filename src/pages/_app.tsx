1e76b23d419a70733e1c1e98ac6a6636841da0c8
Drew Kozmary <2223678+terminaltraces@users.noreply.github.com>
2024-02-22 20:31:40 -0600

    Adding favicons to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout";
 import Head from "next/head";
 
@@ -8,6 +8,19 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/niz4gnx.css" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </Head>
       <ChakraProvider>
         <Layout>
